99f65be3a322a348f4064e1e98ec5a5505b55b75
KevinGomez2021 <93133605+KevinGomez2021@users.noreply.github.com>
2023-03-13 14:25:52 +0100

    iu iu iu

---
+++ joystick_2/joystick_2.ino
@@ -0,0 +1,25 @@
+const byte VRx = A0;
+const byte VRy = A1;
+const byte sw = 5;
+int x,y,b;
+unsigned int temps = 1000;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(VRx, INPUT);
+  pinMode(VRy, INPUT);
+  pinMode(sw, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  x=analogRead(VRx);
+  y=analogRead(VRy);
+  b=digitalRead(sw);
+  Serial.println(x);
+  Serial.println(y);
+  Serial.println(b);
+  delay(temps);
+}
+if(x=>450 && x=<500)
